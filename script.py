dc8cc51948ab094701958129e2f580e233016002
Hari Saputra <saputrahari99@gmail.com>
2020-11-29 13:12:37 +0700

    my app

---
+++ script.py
@@ -0,0 +1,57 @@
+print("Hallo Selamat datang bebey")
+print("-----------------------")
+kondisi = int(input("Sekarang jam berapa iya?(ex: 13, format 24 jam): "))
+
+print(" ")
+
+if 1<=kondisi<=4:
+  print("Kamu belum tidur? Begadang? tidur lah bebey 😢")
+elif 5<=kondisi<=11:
+  print("Selamat Pagi Bebey ❤ ❤ ❤")
+elif 12<=kondisi<=15:
+  print("Selamat Siang Bebey ❤ ❤ ❤")
+elif 16<=kondisi<=18:
+  print("Selamat Sore Bebey ❤ ❤ ❤")
+elif 19<=kondisi<=24:
+  print("Selamat Malam Bebey ❤ ❤ ❤")
+else:
+  print("Masukan jam yang benar dong ❤")
+  print("nggk jadi deh ucap salamnya")
+
+print(" ")
+print(" ")
+
+ngapain = input("Kamu lagi apa bebey? ❤ :")
+print(" ")
+print("ohh, Kalo aku sedang mikirin kamoo ❤ ❤ ❤")
+
+print(" ")
+print(" ")
+
+makan = input("Kamu sudah makan belum bebey? (sudah/belum): ")
+print(" ")
+sudah_makan = "sudah"
+belum_makan = "belum"
+
+if makan == sudah_makan:
+  print("Allhamdulilah bebey 😘")
+elif makan == belum_makan:
+  print("Makan lah bebey 😢")
+else:
+  print("yah dijawab dengan jawaban berbeda :(")
+
+print(" ")
+print(" ")
+
+tanya_makan = input("tanyakan kembali ke saya apakah saya sudah makan atau belum: ")
+print(" ")
+print("Alhamdulillah aku sudah makan bebey")
+
+print(" ")
+print(" ")
+
+print("Have A Good Day bebey? 😘")
+print(" ")
+input("says: ")
+print(" ")
+print("dadahhhhh")
